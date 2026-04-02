1a7c558f54ca9d1e4d3401b75b08a03cd4a91d52
Akash Rathod <akbhau2004@gmail.com>
2026-04-02 10:49:02 +0530

    solve new questiion

--- Patterns/prifixsum.py
+++ Patterns/prifixsum.py
@@ -13,36 +13,140 @@
  
         
 
-def prfix_sum(s,k):
-    sum=0
-    psum=[]
-    for i in range(k,len(s)):
-        sum+=i
-        psum.append(sum)
-    return psum
+# def prfix_sum(s,k):
+#     sum=0
+#     psum=[]
+#     for i in range(k,len(s)):
+#         sum+=i
+#         psum.append(sum)
+#     return psum
 
 
-s=[1,2,3,4,4,3,2,5,5,]
-k=5
-print(prfix_sum(s,k))
-
-
-
+# s=[1,2,3,4,4,3,2,5,5,]
+# k=5
+# print(prfix_sum(s,k))
 
 
 
+# def subarray(nums,k):
+#     count = 0
+#     n=len(nums)
 
-def subarraySum(nums, k):
-    prefix_sum = 0
-    count = 0
-    hashmap = {0: 1}  # important (base case)
+#     for i in range(n):
+#         total = 0
+#         for j in range(i,n):
+#             total+=nums[j]
+#             if total ==k :
+#                 count +=1
+#     return count
+
+# array=[1,2,3,4,2,4,2,5,3,5,9,6,8,7,4,6,6,4]
+# k=23
+# print(subarray(array,k))
+
+
+# def subarraySum(nums, k):
+#     prefix_sum = 0
+#     count = 0
+#     hashmap = {0: 1}  # important (base case)
     
-    for num in nums:
-        prefix_sum += num
+#     for num in nums:
+#         prefix_sum += num
         
-        if (prefix_sum - k) in hashmap:
-            count += hashmap[prefix_sum - k]
+#         if (prefix_sum - k) in hashmap:
+#             count += hashmap[prefix_sum - k]
         
-        hashmap[prefix_sum] = hashmap.get(prefix_sum, 0) + 1
+#         hashmap[prefix_sum] = hashmap.get(prefix_sum, 0) + 1
     
-    return count
+#     return count
+
+
+
+# def subarray(arr):
+#     quries=[(0,2),(1,3)]
+#     prifix=[0]*len(arr)
+#     prifix[0]=arr[0]
+     
+#     for i in range(1,len(arr)):
+#         prifix[i]=prifix[i-1] + arr[i]
+#         result=[]
+#         for i ,j in quries:
+#          if i == 0:
+#             result.append(prifix[j])
+#         else:
+#            result.append(prifix[j]-prifix[i-1])
+
+      
+#     return result
+# arr = [2, 4, 1, 3]
+# print(subarray(arr))
+
+
+
+# def subarray(arr,k):
+#     curr_sum=0
+    
+#     for i in range(len(arr)):
+#         for j in range(i,len(arr)):
+#             curr_sum+=arr[j]
+
+#             if curr_sum == k:
+#                  count+=1
+#     return count
+# arr= [1, 1, 1]
+# k = 2
+# print(subarray(arr,k))
+
+
+
+
+
+
+
+# sum of subarray equals to k
+
+# def subarraySum(nums, k):
+#     prefix_sum = 0
+#     count = 0
+#     hashmap = {0: 1}
+#     for num in nums:
+#         prefix_sum+=num
+#         if(prefix_sum - k) in hashmap:
+#             count+=hashmap[prefix_sum - k]
+#         hashmap[prefix_sum]=hashmap.get(prefix_sum,0)+1
+
+#     return count
+
+
+# arr=[2,3,-5,5,1,4]
+# k=5
+# print(subarraySum(arr,k))
+
+
+
+
+
+def maxlen(arr):
+     n =len(arr)
+     max_length=0
+
+     for i in range(n):
+          current_sum=0
+
+          for j in range(i,n):
+               current_sum+=arr[j]
+
+               if current_sum == 0:
+                    length=j-i+1
+                    max_length=max(max_length,length)
+
+     return max_length
+
+
+arr = [1, 0, -4, 3, 1, 0]
+print(maxlen(arr))
+
+
+
+
+ 
