cc685ea50285cddf47f20e57496ac741767c794a
Akash Rathod <akbhau2004@gmail.com>
2026-04-04 17:14:41 +0530

    new question solved

--- Patterns/hashmap.py
+++ Patterns/hashmap.py
@@ -16,19 +16,56 @@
 
 
 
-def two_sum(arr,target):
-    hashmap={}
+# def two_sum(arr,target):
+#     hashmap={}
 
-    for i in range(len(arr)):
-        complement=target-arr[i]
+#     for i in range(len(arr)):
+#         complement=target-arr[i]
 
-        if complement in hashmap:
-            return[hashmap[complement],i]
-        hashmap[arr[i]]=i
+#         if complement in hashmap:
+#             return[hashmap[complement],i]
+#         hashmap[arr[i]]=i
 
+
+# a=[2,4,6,8]
+# k=10
+# print(two_sum(a,k))
+
+
+
+
+# def show_dubli(arr):
+#     freq={}
+#     result={}
+
+#     for i in range(arr):
+#         freq[i]=freq.get(i,0)+1
+
+#     for key in freq:
+
+#         if freq[key] >1:
+#             result.append(key)
+#         return result
         
-a=[2,4,6,8]
-k=10
-print(two_sum(a,k))
+            
+
+
+
+def count(a, b):
+    n = min(len(a), len(b))
+    hashmap={}
+    count=0
+    
+    for i in range(n):
+        hashmap[i]=hashmap.get(i,0)+1
+        if i in hashmap:
+            count += 1
+    
+    return count
+
+a = [1,2,3,4,5,6,7,8,9,10]
+b = [3,45,6,7,8,4,3,2,1]
+
+print(count(a, b))
 
 
