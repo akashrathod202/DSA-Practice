cb86698d94205925a01951caa191706438f34914
Akash Rathod <akbhau2004@gmail.com>
2026-04-16 23:59:02 +0530

    solve the question of singlylink list

---
+++ Linklist/sll/sllp.py
@@ -0,0 +1,179 @@
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
+
+# class Sll:
+#     def __init__(self):
+#        self.head=None
+    
+#     def insert(self, value):
+#         newnode = Node(value)
+
+
+#         if self.head == None:
+#            self.head=newnode
+#            return
+         
+
+#         newnode.next = self.head   # ✅ link
+#         self.head = newnode 
+
+#     def count(self):
+#             count=0
+
+#             t=self.head
+
+#             while t is not None:  
+#                 count+=1
+#                 t=t.next
+#             print( "the node count is",count) 
+
+#     def search_for(self,x):
+#         temp=self.head
+#         pos=0
+
+#         while temp is not None:
+#             if temp.data == x:
+#                 return pos
+#             temp=temp.next
+#             pos+=1
+#         return -1
+               
+#     def reverse(self):
+#         prev=None
+#         curr=self.head
+
+
+#         while curr is not None:
+#             next=curr.next
+#             curr.next=prev
+#             prev=curr
+#             curr=next
+#         self.head=prev
+
+
+
+        
+
+#     def printall(self):
+#         temp=self.head
+
+#         while temp is not None:
+#             print(temp.data)
+#             temp=temp.next
+
+# obj=Sll()
+# obj.insert(6)
+# obj.insert(8)
+# obj.insert(10)
+# obj.count()
+# obj.search_for(10)
+# obj.reverse()
+# obj.printall()
+
+
+
+
+# class Solution:
+#     def deleteMiddle(self, head, x):
+        
+#         # Case 1: Empty list
+#         if head is None:
+#             return None
+        
+#         # Case 2: If head itself is to be deleted
+#         if head.data == x:
+#             return head.next
+        
+#         temp = head
+        
+#         # Traverse to find the node before the one to delete
+#         while temp.next is not None:
+#             if temp.next.data == x:
+#                 temp.next = temp.next.next
+#                 return head
+            
+#             temp = temp.next
+        
+#         return head   # if value not found
+
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class sll:
+    def __init__(self):
+        self.head = None
+
+    def array(self, arr):
+        if len(arr) == 0:
+            return None
+        
+        self.head = Node(arr[0])
+        temp = self.head
+
+        for i in range(1, len(arr)):
+            newnode = Node(arr[i])
+            temp.next = newnode
+            temp = newnode
+
+    def rearrangeEvenOdd(self):
+        if self.head is None:
+            return
+
+        odd_head = None
+        odd_tail = None
+        even_head = None
+        even_tail = None
+
+        temp = self.head
+
+        while temp is not None:
+            next_node = temp.next   # store next
+
+            if temp.data % 2 != 0:   # odd
+                if odd_head is None:
+                    odd_head = temp
+                    odd_tail = temp
+                else:
+                    odd_tail.next = temp
+                    odd_tail = temp
+            else:   # even
+                if even_head is None:
+                    even_head = temp
+                    even_tail = temp
+                else:
+                    even_tail.next = temp
+                    even_tail = temp
+
+            temp = next_node
+
+        # connect lists
+        if odd_tail is not None:
+            odd_tail.next = even_head
+
+        if even_tail is not None:
+            even_tail.next = None
+
+        self.head = odd_head if odd_head is not None else even_head
+
+    def printall(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.data, end="->")
+            temp = temp.next
+        print("null")
+
+
+# Driver code
+obj = sll()
+arr = [1, 2, 3, 4, 5, 6, 7]
+
+obj.array(arr)
+obj.rearrangeEvenOdd()
+obj.printall()
