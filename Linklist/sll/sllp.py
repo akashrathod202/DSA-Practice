9ca93b89062cd809068c04e44f4057e3c061960f
Akash Rathod <akbhau2004@gmail.com>
2026-04-19 14:27:42 +0530

    solve question of fast and slow pointer

--- Linklist/sll/sllp.py
+++ Linklist/sll/sllp.py
@@ -98,82 +98,240 @@
         
 #         return head   # if value not found
 
+''
+
+
+
+
+
+
+
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+
+# class sll:
+#     def __init__(self):
+#         self.head = None
+
+#     def array(self, arr):
+#         if len(arr) == 0:
+#             return None
+        
+#         self.head = Node(arr[0])
+#         temp = self.head
+
+#         for i in range(1, len(arr)):
+#             newnode = Node(arr[i])
+#             temp.next = newnode
+#             temp = newnode
+
+#     def rearrangeEvenOdd(self):
+#         if self.head is None:
+#             return
+
+#         odd_head = None
+#         odd_tail = None
+#         even_head = None
+#         even_tail = None
+
+#         temp = self.head
+
+#         while temp is not None:
+#             next_node = temp.next   # store next
+
+#             if temp.data % 2 != 0:   # odd
+#                 if odd_head is None:
+#                     odd_head = temp
+#                     odd_tail = temp
+#                 else:
+#                     odd_tail.next = temp
+#                     odd_tail = temp
+#             else:   # even
+#                 if even_head is None:
+#                     even_head = temp
+#                     even_tail = temp
+#                 else:
+#                     even_tail.next = temp
+#                     even_tail = temp
+
+#             temp = next_node
+
+#         # connect lists
+#         if odd_tail is not None:
+#             odd_tail.next = even_head
+
+#         if even_tail is not None:
+#             even_tail.next = None
+
+#         self.head = odd_head if odd_head is not None else even_head
+
+#     def printall(self):
+#         temp = self.head
+#         while temp is not None:
+#             print(temp.data, end="->")
+#             temp = temp.next
+#         print("null")
+
+
+# # Driver code
+# obj = sll()
+# arr = [1, 2, 3, 4, 5, 6, 7]
+
+# obj.array(arr)
+# obj.rearrangeEvenOdd()
+# obj.printall()
+
+
+
+
+
+
+
+# fast and slow pointer  find the middle
+
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
+
+# class sll:
+#     def __init__(self):
+#         self.head=None
+
+#     def inset_at_end(self,value):
+#         newnode=Node(value)
+
+#         if self.head == None:
+#             self.head=newnode
+#             return
+        
+#         temp=self.head
+
+#         while temp.next != None:
+#             temp=temp.next
+        
+#         temp.next=newnode
+
+#         # finding the middle useing the slow and fast pointe
+
+#     def find_mid(self):
+
+#         slow=self.head
+#         fast=self.head
+
+
+#         while fast and fast.next:
+#             slow=slow.next
+#             fast=fast.next.next
+
+#         return slow
+
+
+
+#     def print(self):
+#         if self.head == None:
+#             return
+        
+#         t=self.head
+
+#         while t != None:
+#             print(t.data,end="->")
+#             t=t.next
+
+# obj=sll()
+# obj.inset_at_end(1)
+# obj.inset_at_end(2)
+# obj.inset_at_end(3)
+# obj.inset_at_end(4)
+# obj.inset_at_end(5)
+# obj.inset_at_end(6)
+# obj.inset_at_end(7)
+# obj.print()
+
+# middle=obj.find_mid()
+# print("middle element is :",middle.data)
+        
+            
+
 
 
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
+    def __init__(self,data):
+        self.data=data
+        self.next=None
 
 class sll:
     def __init__(self):
-        self.head = None
+        self.head=None
 
-    def array(self, arr):
-        if len(arr) == 0:
-            return None
-        
-        self.head = Node(arr[0])
-        temp = self.head
+    def inset_at_end(self,value):
+        newnode=Node(value)
 
-        for i in range(1, len(arr)):
-            newnode = Node(arr[i])
-            temp.next = newnode
-            temp = newnode
-
-    def rearrangeEvenOdd(self):
-        if self.head is None:
+        if self.head == None:
+            self.head=newnode
             return
+        
+        temp=self.head
 
-        odd_head = None
-        odd_tail = None
-        even_head = None
-        even_tail = None
+        while temp.next != None:
+            temp=temp.next
+        
+        temp.next=newnode
 
-        temp = self.head
+        # finding the middle useing the slow and fast pointe
 
-        while temp is not None:
-            next_node = temp.next   # store next
+    def find_mid(self):
 
-            if temp.data % 2 != 0:   # odd
-                if odd_head is None:
-                    odd_head = temp
-                    odd_tail = temp
-                else:
-                    odd_tail.next = temp
-                    odd_tail = temp
-            else:   # even
-                if even_head is None:
-                    even_head = temp
-                    even_tail = temp
-                else:
-                    even_tail.next = temp
-                    even_tail = temp
-
-            temp = next_node
-
-        # connect lists
-        if odd_tail is not None:
-            odd_tail.next = even_head
-
-        if even_tail is not None:
-            even_tail.next = None
-
-        self.head = odd_head if odd_head is not None else even_head
-
-    def printall(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.data, end="->")
-            temp = temp.next
-        print("null")
+        slow=self.head
+        fast=self.head
 
 
-# Driver code
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+
+        
+       
+        return slow
+
+
+    def has_cycle(self):
+        slow=self.head
+        fast=self.head
+
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+
+            if slow == fast:
+                return True
+            
+        return False
+
+
+    def print(self):
+        if self.head == None:
+            return
+        
+        t=self.head
+
+        while t != None:
+            print(t.data,end="->")
+            t=t.next
+
 obj = sll()
-arr = [1, 2, 3, 4, 5, 6, 7]
+obj.inset_at_end(1)
+obj.inset_at_end(2)
+obj.inset_at_end(3)
+obj.inset_at_end(4)
+obj.inset_at_end(5)
 
-obj.array(arr)
-obj.rearrangeEvenOdd()
-obj.printall()
+obj.print()
+
+middle = obj.find_mid()
+print("middle element is:", middle.data)
+
+print("Cycle present:", obj.has_cycle())
