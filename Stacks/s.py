9bbc137215ffce66d6c17975ab7df036a351040c
Akash Rathod <akbhau2004@gmail.com>
2026-04-23 00:13:18 +0530

    stck usign linklist

--- Stacks/s.py
+++ Stacks/s.py
@@ -1,42 +1,103 @@
-class stack:
-    def __init__(self):
-        self.s= []
+# class stack:
+#     def __init__(self):
+#         self.s= []
+#         # self represent the current object (instance)
 
     
-    def length(self):
-        return len(self.s)
+#     def length(self):
+#         return len(self.s)
 
-    def push(self,value):
-        self.s.insert(0,value)
+#     def push(self,value):
+#         self.s.insert(0,value)
 
-    def peek(self):
-        if len(self.s)== 0:
-            raise Exception("Stack is empty")
-        else:
-            return self.s[0]
+#     def peek(self):
+#         if len(self.s)== 0:
+#             raise Exception("Stack is empty")
+#         else:
+#             return self.s[0]
         
-    def pop(self):
+#     def pop(self):
 
-        if len(self.s) == 0:
-            raise Exception("Stack is")
+#         if len(self.s) == 0:
+#             raise Exception("Stack is")
          
-        else:
-            return self.s.pop(0)
+#         else:
+#             return self.s.pop(0)
         
 
      
 
-stk=stack()
+# stk=stack()
+# stk.push(10)
+# stk.push(20)
+# stk.push(30)
+
+# # print(stk.peek())
+# print(stk.pop())
+# print(stk.pop())
+# print(stk.pop())
+
+        
+        
+    
+class Node:
+   def __init__(self,data):
+      self.data=data
+      self.next=None
+
+class stack:
+   def __init__(self):
+      self.Top=None
+     
+    
+   def is_empty(self):
+       return self.Top is None
+        
+      
+
+   def push(self,value):
+       temp=Node(value)
+
+       temp.next=self.Top
+       self.Top=temp
+
+   def peek(self):
+       if self.is_empty():
+           raise Exception("stack is empty")
+       return self.Top.data
+   
+   def pop(self):
+      if self.Top is None:
+         raise Exception("stack is empty")
+
+
+      popped=self.Top.data
+      self.Top=self.Top.next
+      return popped
+   
+
+   def display(self):
+      temp=self.Top
+      while temp:
+          print(temp.data,end="->")
+          temp=temp.next
+      print("none")
+
+
+
+stk = stack()
 stk.push(10)
 stk.push(20)
 stk.push(30)
 
-# print(stk.peek())
-print(stk.pop())
-print(stk.pop())
-print(stk.pop())
+stk.display()   # 30 -> 20 -> 10 -> None
 
-        
-        
+print(stk.pop())  # 30
+print(stk.peek()) # 20
+      
+
+
     
-    
+
+
+      
