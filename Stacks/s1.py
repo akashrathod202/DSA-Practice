6d0d80a77851a0927efdf6174adf3335b1498327
Akash Rathod <akbhau2004@gmail.com>
2026-04-25 18:45:01 +0530

    solve the question of  stack good string

--- Stacks/s1.py
+++ Stacks/s1.py
@@ -19,25 +19,53 @@
 
 
 
-def removeOuterParentheses(s):
-    result=''
-    open_count=0
+# def removeOuterParentheses(s):
+#     result=''
+#     open_count=0
 
-    for ch in s:
-        if ch == "(" :
-            if open_count > 0:
-                result+=ch
-            open_count+=1
+#     for ch in s:
+#         if ch == "(" :
+#             if open_count > 0:
+#                 result+=ch
+#             open_count+=1
 
-        else:
-            open_count-=1
-            if open_count > 0:
+#         else:
+#             open_count-=1
+#             if open_count > 0:
 
-                result +=ch
+#                 result +=ch
 
-    return result
+#     return result
     
-s='(()())'
-print(removeOuterParentheses(s))
+# s='(()())'
+# print(removeOuterParentheses(s))
 
 
+
+# check is it goodstring or not
+
+
+def isGoodString(s):
+
+    n=len(s)
+
+    if  n == 1:
+        return True
+    
+
+    for i in range(n-1):
+        diff =abs(ord(s[i]) - ord(s[i+1]))
+        
+
+        dist=min(diff, 26-diff)
+
+        if dist != 1:
+              return"No"
+    return "yes"
+
+        
+
+print(isGoodString("aaa"))  # NO
+print(isGoodString("cbc"))  # YES
+print(isGoodString("ab"))   # YES
+print(isGoodString("az"))
