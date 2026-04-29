4d1c600cf6f97ab97910e6a132e69809f821b268
Akash Rathod <akbhau2004@gmail.com>
2026-04-29 17:53:07 +0530

    solve the question of valid paraenthiess

--- Stacks/s1.py
+++ Stacks/s1.py
@@ -45,27 +45,55 @@
 # check is it goodstring or not
 
 
-def isGoodString(s):
+# def isGoodString(s):
 
-    n=len(s)
+#     n=len(s)
 
-    if  n == 1:
-        return True
+#     if  n == 1:
+#         return True
     
 
-    for i in range(n-1):
-        diff =abs(ord(s[i]) - ord(s[i+1]))
+#     for i in range(n-1):
+#         diff =abs(ord(s[i]) - ord(s[i+1]))
         
 
-        dist=min(diff, 26-diff)
+#         dist=min(diff, 26-diff)
 
-        if dist != 1:
-              return"No"
-    return "yes"
+#         if dist != 1:
+#               return"No"
+#     return "yes"
 
         
 
-print(isGoodString("aaa"))  # NO
-print(isGoodString("cbc"))  # YES
-print(isGoodString("ab"))   # YES
-print(isGoodString("az"))
+# print(isGoodString("aaa"))  # NO
+# print(isGoodString("cbc"))  # YES
+# print(isGoodString("ab"))   # YES
+# print(isGoodString("az"))
+
+
+# balcnced or not
+
+def balenced(s):
+    stack=[]
+    for bracket in s:
+        if bracket == '(' or bracket == "{" or bracket == "[":
+           stack.append(bracket)
+
+        else:
+            if len(stack)== 0:
+                return False
+            else:
+                ch=stack.pop()
+            if( bracket == ')' and ch =='(' 
+            or bracket == '}' and ch =='{' or
+               bracket == ']' and ch =='[' ):
+                
+                continue
+            else:
+                return False
+            
+    return len(stack) == 0
+
+         
+
+print(balenced('((){})'))
