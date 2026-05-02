3cc94a69dbff5cecaf4d0cc7cde1888b53cdcf13
Akash Rathod <akbhau2004@gmail.com>
2026-05-02 11:18:26 +0530

    solve the question of postfix to infix

--- Stacks/expressions/e.py
+++ Stacks/expressions/e.py
@@ -57,68 +57,68 @@
 
 
 
-# infix to  prefix
-class solution:
+# # infix to  prefix
+# class solution:
     
-    def precdedence(self,ch):
-        if ch =="+" or ch == '-':
-            return 1
-        if ch =="*" or ch == "/":
-            return 2
+#     def precdedence(self,ch):
+#         if ch =="+" or ch == '-':
+#             return 1
+#         if ch =="*" or ch == "/":
+#             return 2
         
-        if ch == "^":
-            return 3
-        return 0 
+#         if ch == "^":
+#             return 3
+#         return 0 
     
-    def infixtopostfix(self,s):
+#     def infixtopostfix(self,s):
 
-        s=s[::-1]
-        temp=""
-        for ch in s:
-            if ch ==")":
-               temp+="("
+#         s=s[::-1]
+#         temp=""
+#         for ch in s:
+#             if ch ==")":
+#                temp+="("
 
-            elif ch=="(":
-                temp+=")"
+#             elif ch=="(":
+#                 temp+=")"
 
-            else:
-                 temp+=ch
+#             else:
+#                  temp+=ch
 
 
-        stack=[]
-        result=[]
+#         stack=[]
+#         result=[]
     
 
-        for char in temp :
-            if char.isalnum():
-                result.append(char)
+#         for char in temp :
+#             if char.isalnum():
+#                 result.append(char)
 
-            elif char == "(":
-                 stack.append(char)
+#             elif char == "(":
+#                  stack.append(char)
             
-            elif char ==")":
-                while stack and stack [-1] !=  '(':
-                    result.append(stack.pop())
-                if stack:
-                   stack.pop()
+#             elif char ==")":
+#                 while stack and stack [-1] !=  '(':
+#                     result.append(stack.pop())
+#                 if stack:
+#                    stack.pop()
 
-            else: 
-                while stack and self.precdedence(stack[-1]) >= self.precdedence(char) :
+#             else: 
+#                 while stack and self.precdedence(stack[-1]) >= self.precdedence(char) :
                 
-                    result.append(stack.pop())
-                stack.append(char)
+#                     result.append(stack.pop())
+#                 stack.append(char)
             
 
-        while stack:
-            result.append(stack.pop())
-        return "".join(result[::-1])
+#         while stack:
+#             result.append(stack.pop())
+#         return "".join(result[::-1])
     
 
 
 
-obj=solution()
-obj.precdedence("")
-print(obj.infixtopostfix("a+b*(c-d)"))
+# obj=solution()
+# obj.precdedence("")
+# print(obj.infixtopostfix("a+b*(c-d)"))
 
 
 # def reverse(s):
@@ -136,3 +136,29 @@ print(obj.infixtopostfix("a+b*(c-d)"))
 #     print(temp)
 
 # print(reverse("a+b*(c-d)"))
+
+
+
+# postfix to infix
+
+class solution:
+    def postfix_to_infix(self,s):
+        stack=[]
+        for ch in s:
+            if ch.isalnum():
+               stack.append(ch)
+            
+            else:
+             op1=stack.pop()
+             op2=stack.pop()
+             ans=f"({op2}{ch}{op1})"
+
+             stack.append(ans)
+
+        return stack[-1]
+
+        
+
+obj=solution()
+print(obj.postfix_to_infix('ab-de+f*/')
+)
