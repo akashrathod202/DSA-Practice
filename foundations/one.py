4d20269de195d0e1f2996eb242f0d17460367298
Akash Rathod <akbhau2004@gmail.com>
2026-06-28 12:04:18 +0530

    Implement frequency counter and find maximum frequency

---
+++ foundations/one.py
@@ -0,0 +1,20 @@
+arr = [1,2,3,3,3,4,3,2,1,6,4,53,4]
+
+freq = {}
+
+for i in arr:
+    if i in freq:
+        freq[i] += 1
+    else:
+        freq[i] = 1
+
+large = 0
+element = None
+
+for key, value in freq.items():
+    if value > large:
+        large = value
+        element = key
+
+print("Element:", element)
+print("Frequency:", large)
