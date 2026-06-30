ac945a4c88b2f03af1884fb50c81779fb9209e1b
Akash Rathod <akbhau2004@gmail.com>
2026-06-30 11:08:52 +0530

    Solve the revresive array using two pointer

--- Array/loops.py
+++ Array/loops.py
@@ -15,13 +15,45 @@
 
 # great common diverse
 
-l = 12
-m = 18
+# l = 12
+# m = 18
 
-gcd = 1
+# gcd = 1
 
-for i in range(2, min(l, m) + 1):
-    if l % i == 0 and m % i == 0:
-        gcd = i
+# for i in range(2, min(l, m) + 1):
+#     if l % i == 0 and m % i == 0:
+#         gcd = i
 
-print("The GCD is:", gcd)
+# print("The GCD is:", gcd)
+
+
+# arr=[1,2,3,4,5,6,7]
+# max=arr[0]
+# for i in arr:
+#     if max < arr[i]:
+#         max=arr[i]
+
+# print(max)
+
+
+
+
+# reversed array
+
+arr=[41,25,3,4,5,6,7]
+# revers_arr=arr[::-1]
+# print(revers_arr)
+
+left =0
+right=len(arr)-1
+
+while left < right:
+    arr[left],arr[right]=arr[right],arr[left]
+    left+=1
+    right-=1
+print(arr)
+    
+
+    
+    
+ 
