263b40ae72c5665cbac0741670050f6408129503
Akash Rathod <akbhau2004@gmail.com>
2026-07-11 10:59:14 +0530

    optimaize the three sum problem with time compexity n2

--- Patterns/twopointer.py
+++ Patterns/twopointer.py
@@ -132,23 +132,41 @@
 # print(movez(arr))
           
 
-arr = [-1, 0, 1, 2, -1, -4]
-target = 0
+# arr = [-1, 0, 1, 2, -1, -4]
+# target = 0
 
-n = len(arr)
-my_set = set()
+# n = len(arr)
+# my_set = set()
 
-for i in range(n):
-    for j in range(i + 1, n):
-        for k in range(j + 1, n):
-            if arr[i] + arr[j] + arr[k] == target:
-                temp = [arr[i], arr[j], arr[k]]  # Use a list
-                temp.sort()                      # Lists can be sorted
-                my_set.add(tuple(temp))          # Convert to tuple for the set
+# for i in range(n):
+#     for j in range(i + 1, n):
+#         for k in range(j + 1, n):
+#             if arr[i] + arr[j] + arr[k] == target:
+#                 temp = [arr[i], arr[j], arr[k]]  # Use a list
+#                 temp.sort()                      # Lists can be sorted
+#                 my_set.add(tuple(temp))          # Convert to tuple for the set
 
-result = [list(ans) for ans in my_set]
-print(result)
+# result = [list(ans) for ans in my_set]
+# print(result)
 
+
+
+# implement the solution with time complexity n squre
+
+arr = [-1,0,1,2,-1,-4]
+n=len(arr)
+result=set()
+for i in range(0,n):
+    my_set=set()
+    for j in range(i+1,n):
+        third=-arr[i]+arr[j]
+        if third in my_set:
+            temp=[arr[i],arr[j]]
+            temp.sort()
+            result.add(tuple(temp))
+        my_set.add(arr[j])
+ans=[list(ans) for ans in result]
+print(ans)
 
 
 
