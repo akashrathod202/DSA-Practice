571deffe7dd35ddfe13940d8041e49e370c7bbef
Akash Rathod <akbhau2004@gmail.com>
2026-07-13 10:17:25 +0530

    solve the question of maximum sum subarray of size k using sliding window

---
+++ Patterns/slidingwinow.py
@@ -0,0 +1,12 @@
+# Maximum Sum Subarray of Size K (Sliding Window)
+def max_subarry(arr,k):
+    window_sum=sum(arr[:k])
+    max_sum=window_sum
+
+    for  i in range(k,len(arr)):
+        window_sum+=arr[i]-arr[i-k]
+        max_sum=max(max_sum,window_sum)
+    return max_sum
+
+arr=[2,1,5,1,3,2]
+print(max_subarry(arr,3))
