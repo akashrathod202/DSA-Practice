341c28df1aea82d3dedade54feb3f8585101fb66
Akash Rathod <akbhau2004@gmail.com>
2026-07-15 10:04:34 +0530

    Solve linear search question

--- foundations/one.py
+++ foundations/one.py
@@ -1,20 +1,33 @@
-arr = [1,2,3,3,3,4,3,2,1,6,4,53,4]
+# arr = [1,2,3,3,3,4,3,2,1,6,4,53,4]
 
-freq = {}
+# freq = {}
 
-for i in arr:
-    if i in freq:
-        freq[i] += 1
-    else:
-        freq[i] = 1
+# for i in arr:
+#     if i in freq:
+#         freq[i] += 1
+#     else:
+#         freq[i] = 1
 
-large = 0
-element = None
+# large = 0
+# element = None
 
-for key, value in freq.items():
-    if value > large:
-        large = value
-        element = key
+# for key, value in freq.items():
+#     if value > large:
+#         large = value
+#         element = key
 
-print("Element:", element)
-print("Frequency:", large)
+# print("Element:", element)
+# print("Frequency:", large)
+
+def check(arr,k):
+     for i in range(0,len(arr)):
+        if arr[i] == k:
+          return i,arr[i]
+          
+     return -1
+     
+ 
+
+arr= [5, 8, 2, 10, 6]
+print(check(arr,8))
+
