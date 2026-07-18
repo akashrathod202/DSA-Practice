c0d00e0c640a6f6a7b370a8d33d3fdced2aec044
Akash Rathod <akbhau2004@gmail.com>
2026-07-19 00:15:08 +0530

    implement the logic of palindrome using two pointer

--- Strings/basic .py
+++ Strings/basic .py
@@ -9,3 +9,27 @@ def prints(s):
 
 s = "AKASH"
 prints(s)
+
+
+
+
+def palindrome(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        if arr[left] != arr[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+
+s = "madam"
+
+if palindrome(s):
+    print("This is a palindrome")
+else:
+    print("This is not a palindrome")
