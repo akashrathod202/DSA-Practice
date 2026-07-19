6e0cbfc12b9474982424ecb5eba3059c39d9e2dd
Akash Rathod <akbhau2004@gmail.com>
2026-07-19 11:20:41 +0530

    implement the logic of remove space from string

--- Strings/basic .py
+++ Strings/basic .py
@@ -13,23 +13,36 @@ prints(s)
 
 
 
-def palindrome(arr):
-    left = 0
-    right = len(arr) - 1
+# def palindrome(arr):
+#     left = 0
+#     right = len(arr) - 1
 
-    while left < right:
-        if arr[left] != arr[right]:
-            return False
+#     while left < right:
+#         if arr[left] != arr[right]:
+#             return False
 
-        left += 1
-        right -= 1
+#         left += 1
+#         right -= 1
 
-    return True
+#     return True
 
 
-s = "madam"
+# s = "madam"
 
-if palindrome(s):
-    print("This is a palindrome")
-else:
-    print("This is not a palindrome")
+# if palindrome(s):
+#     print("This is a palindrome")
+# else:
+#     print("This is not a palindrome")
+
+
+
+def remove_space(arr):
+    result=[]
+    for i in arr:
+        if i != " ":
+           result.append(i)
+        
+    return "".join(result)
+     
+arr='akash rathod'
+print(remove_space(arr))
