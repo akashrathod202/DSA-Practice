1620872f1559d54ef14fd629ec3133a99c4058d6
Akash Rathod <akbhau2004@gmail.com>
2026-07-28 15:18:14 +0530

    implement the logic  of finding the dublicate element using hasing

--- Hasing/h1.py
+++ Hasing/h1.py
@@ -1,8 +1,18 @@
-s='banana'
-has={}
-for ch in s:
-    if ch  not in has:
-        has[ch]=1
-    else:
-        has[ch]+=1
-print(has)
+# s='banana'
+# has={}
+# for ch in s:
+#     if ch  not in has:
+#         has[ch]=1
+#     else:
+#         has[ch]+=1
+# print(has)
+
+arr=[11,3,4,2,3]
+seen=set()
+for i in arr:
+    if i in seen:
+        print(i)
+        break
+    seen.add(i)
+print(seen)
+        
