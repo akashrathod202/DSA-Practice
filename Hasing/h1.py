56b97584c2cfaa1e6bca7cb0ebeb165580b6e973
Akash Rathod <akbhau2004@gmail.com>
2026-07-28 16:09:11 +0530

    implement the logic of two_sum using hasing

--- Hasing/h1.py
+++ Hasing/h1.py
@@ -7,12 +7,33 @@
 #         has[ch]+=1
 # print(has)
 
-arr=[11,3,4,2,3]
-seen=set()
-for i in arr:
-    if i in seen:
-        print(i)
-        break
-    seen.add(i)
-print(seen)
+
+# arr=[11,3,4,2,3]
+# seen=set()
+# for i in arr:
+#     if i in seen:
+#         print(i)
+#         break
+#     seen.add(i)
+# print(seen)
+
+
+# two_sum
+ 
+
+
+def two_pointer(nums,target):
+    seen={}
+    for i,num in enumerate(nums):
+        compliment=target-num
+        if compliment in seen:
+            return [seen[compliment],i]
+        seen[num]=i
+    
+
+    
+
+arr=[2,7,4,8,34]
+print(two_pointer(arr,9))
+
         
