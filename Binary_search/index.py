4efc94a7c8ab217747b6d7e708c33078872af8d4
Akash Rathod <akbhau2004@gmail.com>
2026-08-15 11:11:25 +0530

    solve binary search element and occurrence problems

--- Binary_search/index.py
+++ Binary_search/index.py
@@ -1,19 +1,82 @@
-def binary_search(arr,target):
-    left=0
-    right=len(arr)-1
+# def binary_search(arr,target):
+#     left=0
+#     right=len(arr)-1
 
-    while left <= right:
-        mid=(left + right) // 2
+#     while left <= right:
+#         mid=(left + right) // 2
+
+#         if arr[mid] == target:
+#             return mid
+        
+#         elif arr[mid] < target:
+#             left = mid + 1
+
+#         else :
+#             right=mid - 1
+
+# arr = [2, 4, 6, 8, 10, 12, 14]
+
+# print(binary_search(arr, 12))
+
+
+
+
+# first_occurrence
+
+# def first_occurrence(arr,target):
+#     low=0
+#     high=len(arr)-1
+#     answer=-1
+
+
+#     while  low <= high:
+#         mid=(low + high) // 2
+
+#         if arr[mid] == target:
+#             answer=mid
+#             high=mid-1
+
+
+#         elif arr[mid] < target:
+#             low=mid+1
+
+#         else:
+#             high=mid-1
+
+#     return answer
+
+# arr = [1, 2, 2, 2, 4, 5]
+ 
+# print(first_occurrence(arr,2))
+
+
+# last_occurence
+
+def last_occurence(arr,target):
+    low=0
+    high=len(arr)-1
+    answer=-1
+
+
+    while low <= high:
+        mid=(low + high) // 2
 
         if arr[mid] == target:
-            return mid
-        
+            answer=mid
+            low=mid+1
+
         elif arr[mid] < target:
-            left = mid + 1
+            low=mid+1
 
-        else :
-            right=mid - 1
+        else:
+             high = mid - 1
+    return answer
 
-arr = [2, 4, 6, 8, 10, 12, 14]
+             
 
-print(binary_search(arr, 12))
+arr = [1, 2, 2, 2, 4, 5]
+print(last_occurence(arr,2))
+
+
+
+
