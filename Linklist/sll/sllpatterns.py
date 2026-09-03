509afc1768c1f312bf6b9e6f9e2dbd9745cc5aa6
Akash Rathod <akbhau2004@gmail.com>
2026-09-03 15:55:29 +0530

    implement the logic of find the begining of cycle

--- Linklist/sll/sllpatterns.py
+++ Linklist/sll/sllpatterns.py
@@ -40,7 +40,7 @@ class SinglyLinkedList:
             print("the list empty")
             return
         
-        self.tail.next=self.head.next.next
+        self.tail.next=self.head.next.next.next
 
     
     def detect_cycle(self):
@@ -64,7 +64,111 @@ class SinglyLinkedList:
         print("not detect")
         return False
 
+    def del_postions(self,postion):
 
+        if self.head is None:
+            print("list is empty")
+            return
+        
+        if postion == 0:
+            self.head=self.head.next
+            return
+        
+        prv=None
+        current=self.head
+        index=0
+
+        while current is not  None:
+            if postion == index:
+                prv.next=current.next
+                return
+                
+            
+
+            prv=current
+            index+=1
+            current=current.next
+        print("postion doesnot exitsts")
+            
+
+    def delete_byvalue(self,value):
+        if self.head is None:
+            print("the list is empty")
+            return
+        
+        current=self.head
+        prev=None
+        while current is not None:
+            if current.data==value:
+                prev.next=current.next
+            
+            prev=current
+            current=current.next
+        print("value doesnot exsits in node")
+
+                
+
+    def del_nthnode_fromlast(self):
+        if self.head is None:
+            print("list is empty ")
+            return
+        
+        temp=self.head
+        prev=None
+        while temp.next is not None:
+            prev=temp
+            temp=temp.next
+        print("we reached",temp.data)
+        print("and the prev is",prev.prev.prev.data)
+
+        # while prev.prev != self.head:
+        #     prev=prev.prev
+        #     print(temp.data)
+        # print("we back to head",prev.data)
+
+    def want_list(self,n):
+        if self.head == None:
+            print("list is empty")
+            return[]
+        temp=self.head
+         
+        list=[]
+        while temp.next is not None:
+            if temp.data != n:
+                 list.append(temp.data)
+                  
+             
+
+          
+            temp=temp.next
+            
+                
+        return list
+
+
+
+       
+
+    def check_start(self):
+       
+
+        if self.head is None:
+            print("the list is empty")
+            return
+        visted=()
+        temp=self.head
+
+        while temp:
+            if temp in visted:
+                 return temp
+            
+            visted.add(temp)
+
+            temp=temp.next
+                
+        print("we got")
+
+ 
     def display(self):
         current = self.head
 
@@ -85,7 +189,13 @@ ll.insert(40)
 ll.insert(50)
 ll.insert(60)
 # print(ll.middle())
-# ll.create_loop()
+ll.create_loop()
+# ll.del_postions(29)
+# ll.del_nthnode_fromlast()
+# ll.delete_byvalue(509)
+# print(ll.want_list(40))
+print(ll.check_start())
 # ll.display()
-print(ll.detect_cycle())
+# print(ll.detect_cycle())
+
  
