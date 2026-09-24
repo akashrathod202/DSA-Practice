6d377ca1bcf222d0e1e715aeb374b5d99c0ba183
Akash Rathod <akbhau2004@gmail.com>
2026-09-24 17:15:05 +0530

    solve the problem merge two sorted list

--- Linklist/sll/sllpatterns.py
+++ Linklist/sll/sllpatterns.py
@@ -246,3 +246,123 @@ ll.reverse()
 # print(ll.detect_cycle())
 
  
+ class node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Singlylinklist:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, data):
+        new_node = node(data)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def mergelist(self, l):
+        if self.head is None:
+            print("The first list is empty")
+            return
+
+        if l.head is None:
+            print("The second list is empty")
+            return self.head
+
+        p1 = self.head
+        p2 = l.head
+
+        dummy = node(0)
+        temp = dummy
+
+        while p1 is not None and p2 is not None:
+
+            if p1.data <= p2.data:
+                temp.next = p1
+                p1 = p1.next
+            else:
+                temp.next = p2
+                p2 = p2.next
+
+            temp = temp.next
+
+        # Attach remaining nodes
+        if p1 is not None:
+            temp.next = p1
+        else:
+            temp.next = p2
+
+        return dummy.next
+
+    def display(self, head=None):
+
+        # If no head is provided, display this list's head
+        if head is None:
+            head = self.head
+
+        if head is None:
+            print("This list is empty")
+            return
+
+        temp = head
+
+        while temp is not None:
+            print(temp.data, end="-->")
+            temp = temp.next
+
+        print("None")
+
+
+# -------------------------
+# List 1
+# -------------------------
+
+ll = Singlylinklist()
+
+ll.insert(12)
+ll.insert(40)
+ll.insert(89)
+
+
+# -------------------------
+# List 2
+# -------------------------
+
+ll2 = Singlylinklist()
+
+ll2.insert(12)
+ll2.insert(40)
+ll2.insert(89)
+
+
+# -------------------------
+# Display original lists
+# -------------------------
+
+print("List 1:")
+ll.display()
+
+print("List 2:")
+ll2.display()
+
+
+# -------------------------
+# Merge
+# -------------------------
+
+merged_head = ll.mergelist(ll2)
+
+
+# -------------------------
+# Display merged list
+# -------------------------
+
+print("Merged List:")
+ll.display(merged_head)
